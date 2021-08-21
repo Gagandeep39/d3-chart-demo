efe11a95830b7f675da5896ca56aad4ad25c7859
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-08-21 21:24:55 +0530

    Added margins

--- 05_d3-linear-scale/script.js
+++ 05_d3-linear-scale/script.js
@@ -4,20 +4,31 @@ const svg = d3
   .select('.canvas')
   .append('svg')
   .attr('width', 600)
-  .attr('height', 600)
-  .style('background-color', 'gray');
+  .attr('height', 600);
+
+// Create margins
+const margin = {
+  top: 20,
+  right: 20,
+
+  bottom: 100,
+  left: 100,
+};
+
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
 
 d3.json('05_d3-linear-scale/menu.json').then((data) => {
   const min = d3.min(data, (d) => d.orders); // Find min
   const max = d3.max(data, (d) => d.orders); // Find max
   const extent = d3.extent(data, (d) => d.orders); // FInds min, max
 
-  const y = d3.scaleLinear().domain([0, max]).range([0, 600]);
+  const y = d3.scaleLinear().domain([0, max]).range([0, graphHeight]);
 
   const x = d3
     .scaleBand()
     .domain(data.map((i) => i.name))
-    .range([0, 600])
+    .range([0, graphWidth])
     .paddingInner(0.3)
     .paddingOuter(0.1);
 
