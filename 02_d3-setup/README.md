e23976bf636ca5339ce4a0e9a8da6fe0fdb85b60
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-08-21 14:19:41 +0530

    Created basic shapes using d3 svg

---
+++ 02_d3-setup/README.md
@@ -0,0 +1,19 @@
+# D3 Basics
+
+- [D3 Basics](#d3-basics)
+  - [Deployment](#deployment)
+  - [Description](#description)
+  - [Screenshots](#screenshots)
+
+## Deployment
+
+- Checkout deployment at <https://gagandeep39.github.io/d3-basics/>
+
+## Description
+
+- D3 basics
+
+## Screenshots
+
+![Screenshot 1](./assets/screenshot_1.png)
+![Screenshot 2](./assets/screenshot_2.png)
---
+++ 02_d3-setup/script.js
@@ -0,0 +1,47 @@
+import * as d3 from 'https://cdn.skypack.dev/d3@7';
+
+const canvas = d3.select('.canvas');
+const svg = canvas.append('svg');
+svg.attr('width', 600);
+svg.attr('height', 600);
+// svg.attr('fill', 'gray'); // Doesnt work, Can only be used on element isnie svf
+svg.style('background-color', '#f0f0f0');
+
+// Create and append to rect
+// const rect = svg.append('rect');
+// rect.append('circle');
+
+// Append shapes to svg conainer
+svg
+  .append('rect')
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr('fill', 'blue')
+  .attr('x', 20)
+  .attr('y', 20);
+
+svg
+  .append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .attr('fill', 'pink');
+svg
+  .append('line')
+  .attr('x1', 370)
+  .attr('x2', 400)
+  .attr('y1', 20)
+  .attr('y2', 120)
+  .attr('stroke', 'red');
+
+svg
+  .append('text')
+  .attr('x', 20)
+  .attr('y', 200)
+  .text('Hello World')
+  .attr('fill', 'gray')
+  .style('font-size', '20px');
+
+// Mehod changing
+// Adds attribut to previously returned value
+// rect.attr('width', 100).attr('height', 100).attr('fill', 'red'); will create a rect with height and width and color in  a single line
